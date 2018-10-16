728578e637606b25bb26772cc08d16e5561a90b4
heifade <heifade@126.com>
2018-10-16 20:03:52 +0800

    no message

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
-import typescript from 'rollup-plugin-typescript';
+import typescript from "rollup-plugin-typescript";
 
 export default {
   input: "src/index.ts",
@@ -14,10 +14,16 @@ export default {
   plugins: [
     typescript(),
     json(),
-    resolve(),
+    resolve({
+      // 将自定义选项传递给解析插件
+      customResolveOptions: {
+        moduleDirectory: "node_modules"
+      }
+    }),
     commonjs(),
     babel({
       exclude: "node_modules/**"
     })
-  ]
+  ],
+  external: ["lodash-es"]
 };
